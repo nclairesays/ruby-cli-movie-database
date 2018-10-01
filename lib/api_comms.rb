3e78f60abf16f7053d7bf61f887778a7be7e99ee
ssong-eu <sangyoon.song@gmail.com>
2018-10-01 17:53:02 +0100

    added api_comm

---
+++ lib/api_comms.rb
@@ -0,0 +1,27 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
+##### Makes an API call based on the user input "title"
+##### Saves the movie information to the database and
+##### and returns the relevant movie object
+def get_movie_from_api(user_input)
+
+  url = "http://www.omdbapi.com/?apikey=5c74cb50&t=#{user_input}"
+
+  response_string = RestClient.get(url)
+  response = JSON.parse(response_string)
+
+  Movie.new(title: response["Title"],
+            year: response["Year"],
+            rated: response["Rated"],
+            director: response["Director"],
+            plot: response["Plot"],
+            imdb_score: response["imdbRating"])
+
+  # Genre.new(genre: response["Genre"])
+  # Actor.new() need to split and iterate and split
+
+end
+
