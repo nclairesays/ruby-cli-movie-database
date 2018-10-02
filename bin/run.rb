b78f6d1e7b56186c83eed430ffd9c769cd3f5bad
ssong-eu <sangyoon.song@gmail.com>
2018-10-03 00:14:16 +0100

    made minor changes

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,10 @@
 require_relative '../config/environment'
 # require 'tty-command'
+#
+# cmd = TTY::Command.new
+# script = 'tell app "Terminal"
+#     do script "ruby ~./bin/start.rb"
+# end tell'
+# cmd.run("osascript -e", script)
 
 CLI.welcome
